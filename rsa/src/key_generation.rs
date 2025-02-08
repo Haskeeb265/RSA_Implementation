7c9776398d964bbe4981312cdc182643d533a894
Haseebullah <haskeebullah265@gmail.com>
2025-02-09 04:06:18 +0500

    Generated 2 prime numbers p and q

--- rsa/src/key_generation.rs
+++ rsa/src/key_generation.rs
@@ -1,16 +1,26 @@
-use rand::Rng;
+use rand::Rng; //Generates random numbers. Imported Rng from rand crate
+use rand::thread_rng; //Initializes the random number generator for the current thread
+use num_prime::RandPrime; //Used to generate prime numbers and apply prime functions
 
-pub fn prime_num_generator(){
-    let mut rng = rand::thread_rng();
-    let p: u64 = rng.gen();
-    let q: u64 = rng.gen();
+pub fn generating_first_prime() -> u64 { //When we want to return a single value
 
-    
+    let mut rng = thread_rng();
+    rng.gen_prime(64) //We don't use semi-colons when we want to return a value.
 
 }
 
-pub fn computing_n(){
 
+pub fn generating_second_prime() -> (u64,u64) { //when we want to return multiple values
 
+    let p = generating_first_prime();
+    let mut q;
 
+    loop{ //Continuously perform the below 2 tasks until the condition is met
+        q = generating_first_prime(); //generate a prime value
+        if p != q{ //Check if the the prime value generated is not equal to p
+            break;
+        }
+    }
+
+    (p,q) //returning p and q
 }
