9f7687fa167abbc1500438a044ff26ef82bd575e
Haseebullah <haskeebullah265@gmail.com>
2025-02-09 17:20:40 +0500

    Finished function of private key generation

--- rsa/src/key_generation.rs
+++ rsa/src/key_generation.rs
@@ -1,5 +1,6 @@
 use num_primes::Generator; //This library is used to generate random prime numbers.
 use num_bigint::BigUint; //This is to deal with large numbers
+use num_integer::Integer; //This is to implement modular arthematic
 
 pub fn two_prime_generator() -> (num_primes::BigUint, num_primes::BigUint) { //Specifying that the reurn value of this function are 2 values of type BigUint
 
@@ -13,10 +14,18 @@ pub fn two_prime_generator() -> (num_primes::BigUint, num_primes::BigUint) { //S
     (p,q) //return p and q
 }
 
-
-
-pub fn calculating_n (p: BigUint, q: BigUint) -> BigUint{ //Taking p and q of type BigUint as input parameters and returning a value of type BigUint
+pub fn calculating_n(p: BigUint, q: BigUint) -> BigUint{ //Taking p and q of type BigUint as input parameters and returning a value of type BigUint
     
     p*q //Product of p and q
 
+}
+
+pub fn euler_totient(p:BigUint, q: BigUint) -> BigUint{
+   let et = (p - BigUint::from(1u64)) * (q - BigUint::from(1u64));
+   et //Stored euler's totient in variable "et"
+}
+
+// Calculate the modular inverse of e modulo φ(n)
+pub fn private_key(e: BigUint, et: BigUint) -> BigUint {
+    e.modinv(&et).expect("Modular inverse does not exist!") //& sign is used for referencing.
 }
