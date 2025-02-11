f38b2004cdbf405b5bd9591cd059eebc3f931fca
Haseebullah <haskeebullah265@gmail.com>
2025-02-12 04:05:08 +0500

    Final Commit (Without padding)

--- rsa/src/decryption.rs
+++ rsa/src/decryption.rs
@@ -4,5 +4,5 @@ use std::string::FromUtf8Error;
 pub fn decryption(c: &BigUint, n: &BigUint, d: &BigUint) -> Result<String, FromUtf8Error> {
     let m = c.modpow(d, n);
     let message = m.to_bytes_be();
-    String::from_utf8_lossy(message)
+    String::from_utf8(message)
 }
--- rsa/src/main.rs
+++ rsa/src/main.rs
@@ -9,7 +9,13 @@ fn main() {
     let et = key_generation::euler_totient(p.clone(), q.clone());
     let e = BigUint::from(65537u64);
     let d = key_generation::private_key(e, et.clone());
-    let encrpyt = encryption::encryption("Hello World!", &n);
+    let encrpyt = encryption::encryption("Hello Haseeb!", &n);
+
+    println!("Random Primes: {},{}", p, q);
+    println!("Euler Totient: {}", et);
+    println!("n = : {}", n);
+    println!("Private Key: {}", d);
+    println!("Encrypted Message: {}", encrpyt);
 
     match decryption::decryption(&encrpyt, &n, &d) {
         Ok(decrypt) => println!("Decrypted Message: {}", decrypt),
@@ -21,10 +27,4 @@ fn main() {
             println!("Raw decrypted bytes: {:?}", raw_bytes);
         },
     }
-
-    println!("Random Primes: {},{}", p, q);
-    println!("Euler Totient: {}", et);
-    println!("n = : {}", n);
-    println!("Private Key: {}", d);
-    println!("Encrypted Message: {}", encrpyt);
 }
